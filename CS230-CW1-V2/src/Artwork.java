ccbeddaaef6b3995857aefa82a884a2874d61812
ABallard98 <aydenballard@gmail.com>
2017-11-15 19:01:33 +0000

    Started adding comments
    added javadoc comments to artwork and user classes

--- CS230-CW1-V2/src/Artwork.java
+++ CS230-CW1-V2/src/Artwork.java
@@ -6,17 +6,26 @@ import java.util.*;
 
 public class Artwork {
 
-    private User seller;
-    private Date dateAdded;
-    private String name;
-    private String description;
-    private String creator;
-    private int yearWasMade;
-    private int numberOfBids;
-    private double reservePrice;
+    private User seller; //seller of the artwork
+    private Date dateAdded; //date added to the system
+    private String name; //name of the artwork
+    private String description; //description of the artwork
+    private String creator; //name of creator of the artwork
+    private int yearWasMade; //year the artwork was made
+    private int numberOfBids; //number of the bids possible for this artwork
+    private double reservePrice; //reserve price for the artwork
+    private ArrayList<Bid> bids; //ArrayList of bids on this artwork
 
-    private ArrayList<Bid> bids;
-
+    /**
+     * Constructor of artwork without a description
+     * @param seller - seller of the artwork
+     * @param date - date added to the system
+     * @param name - name of the artwork
+     * @param creator - creator of the artwork
+     * @param yearWasMade - year the artwork was made
+     * @param numberOfBids - number of bids possible for the artwork
+     * @param reservePrice - reserve price of the artwork
+     */
     public Artwork(User seller, Date date, String name, String creator, int yearWasMade, int numberOfBids,
                    double reservePrice){
         this.seller = seller;
@@ -29,6 +38,17 @@ public class Artwork {
         this.bids = new ArrayList<>();
     }
 
+    /**
+     * Constructor of artwork with a  a description
+     * @param seller - seller of the artwork
+     * @param date - date added to the system
+     * @param name - name of the artwork
+     * @param creator - creator of the artwork
+     * @param yearWasMade - year the artwork was made
+     * @param numberOfBids - number of bids possible for the artwork
+     * @param reservePrice - reserve price of the artwork
+     * @param description  - description of the artwork
+     */
     public Artwork(User seller, Date date, String name, String creator, int yearWasMade, int numberOfBids,
                    float reservePrice, String description){
         this.seller = seller;
@@ -42,70 +62,138 @@ public class Artwork {
         this.description = description;
     }
 
+    /**
+     * Method to get the seller of the artwork
+     * @return User - seller
+     */
     public User getSeller(){
         return this.seller;
     }
 
+    /**
+     * Method to get the date the artwork was added to the system
+     * @return Date - dateAdded
+     */
     public Date getDateAdded(){
         return this.dateAdded;
     }
 
+    /**
+     * Method to get the name of the artwork
+     * @return String - name
+     */
     public String getName(){
         return this.name;
     }
 
+    /**
+     * Method to get the description of the artwork
+     * @return String - description
+     */
     public String getDescription(){
         return this.description;
     }
 
+    /**
+     * Method to get the creator of the artwork
+     * @return String - creator
+     */
     public String getCreator(){
         return this.creator;
     }
 
+    /**
+     * Method to get the year the artwork was made
+     * @return int - year
+     */
     public int getYearWasMade(){
         return this.yearWasMade;
     }
 
+    /**
+     * Method to get the number of bids possible on the artwork
+     * @return int - numberOfBids
+     */
     public int getNumberOfBids(){
         return this.numberOfBids;
     }
 
+    /**
+     * Method to get the reserve price of the artwork
+     * @return double - reservePrice
+     */
     public double getReservePrice(){
         return this.reservePrice;
     }
 
+    /**
+     * Method to set the seller of the artwork
+     * @param seller
+     */
     public void setSeller(User seller){
         this.seller = seller;
     }
 
+    /**
+     * Method to set the date added of the artwork
+     * @param date
+     */
     public void setDateAdded(Date date){
         this.dateAdded = date;
     }
 
+    /**
+     * Method to set the name of the artwork
+     * @param name
+     */
     public void setName(String name){
         this.name = name;
     }
 
+    /**
+     * Method to set the description of the artwork
+     * @param description
+     */
     public void setDescription(String description){
         this.description = description;
     }
 
+    /**
+     * Method to set the creator of the artwork
+     * @param creator
+     */
     public void setCreator(String creator){
         this.creator = creator;
     }
 
+    /**
+     * Method to set the year the artwork was made
+     * @param year
+     */
     public void setYearWasMade(int year){
         this.yearWasMade = year;
     }
 
+    /**
+     * Method to set the number of bids possible on the artwork
+     * @param bids
+     */
     public void setNumberOfBids(int bids){
         this.numberOfBids = bids;
     }
 
+    /**
+     * Method to set the reservePrice of the artwork
+     * @param price
+     */
     public void setReservePrice(double price){
         this.reservePrice = price;
     }
 
+    /**
+     * Method to place a bid on the artwork
+     * @param bid
+     */
     public void placeBid(Bid bid){
         if(bids.size() < numberOfBids && bid.getPrice() > bids.get(bids.size()-1).getPrice()){
             bids.add(bid);
--- CS230-CW1-V2/src/User.java
+++ CS230-CW1-V2/src/User.java
@@ -4,116 +4,217 @@
 
 import java.util.*;
 
-public class User
-{
+public class User {
 
-    private String username;
-    private String firstName;
-    private String lastName;
-    private String address;
-    private String postcode;
-    private long phonenumber;
+    private String username; //username of the user
+    private String firstName; //first name of the user
+    private String lastName; //last name of the user
+    private String address; //address of the user
+    private String postcode; //post code of the user
+    private long phonenumber; //phone number of the user
 
-    private ArrayList <User> favouriteUsers;
+    private ArrayList<User> favouriteUsers; //array list of favourite users
 
-    public User(String username, String fname, String lname, String address, String postcode, long phonenumber){
+    /**
+     * Constructor for user object
+     *
+     * @param username    - username of the user
+     * @param fname       - first name of the user
+     * @param lname       - last name of the user
+     * @param address     - address of the user
+     * @param postcode    - postcode of the user
+     * @param phonenumber - phonenumber of the user
+     */
+    public User(String username, String fname, String lname, String address, String postcode, long phonenumber) {
         this.username = username;
         this.firstName = fname;
         this.lastName = lname;
         this.address = address;
         this.postcode = postcode;
         this.phonenumber = phonenumber;
-
         this.favouriteUsers = new ArrayList<>();
     }
 
-    public String getUsername(){
+    /**
+     * Method to get the user's username
+     *
+     * @return username
+     */
+    public String getUsername() {
         return this.username;
     }
 
-    public String getFirstName(){
+    /**
+     * Method to get the user's first name
+     *
+     * @return fistname
+     */
+    public String getFirstName() {
         return this.firstName;
     }
 
-    public String getLastName(){
+    /**
+     * Method to get the user's last name
+     *
+     * @return lastname
+     */
+    public String getLastName() {
         return this.lastName;
     }
 
-    public String getFullName(){
+    /**
+     * Method to get the user's fullname
+     *
+     * @return full name of the user
+     */
+    public String getFullName() {
         return this.firstName + " " + this.lastName;
     }
 
-    public String getAddress(){
+    /**
+     * Method to get the user's address
+     *
+     * @return address
+     */
+    public String getAddress() {
         return this.address;
     }
 
-    public String getPostcode(){
+    /**
+     * Method to get the user's postcode
+     *
+     * @return postcode
+     */
+    public String getPostcode() {
         return this.postcode;
     }
 
-    public long getPhonenumber(){
+    /**
+     * Method to get the user's phone number
+     *
+     * @return phone number
+     */
+    public long getPhonenumber() {
         return this.phonenumber;
     }
 
-    public void setUsername(String username){
+    /**
+     * Method to set the user's username
+     *
+     * @param username
+     */
+    public void setUsername(String username) {
         this.username = username;
     }
 
-    public void setFirstName(String fname){
+    /**
+     * Method to set the user's first name
+     *
+     * @param fname
+     */
+    public void setFirstName(String fname) {
         this.firstName = fname;
     }
 
-    public void setLastName(String lname){
+    /**
+     * Method to set the user's last name
+     *
+     * @param lname
+     */
+    public void setLastName(String lname) {
         this.lastName = lname;
     }
 
-    public void setAddress(String address){
+    /**
+     * Method to set the user's address
+     *
+     * @param address
+     */
+    public void setAddress(String address) {
         this.address = address;
     }
 
-    public void setPostcode(String postcode){
+    /**
+     * Method to set the user's postcode
+     *
+     * @param postcode
+     */
+    public void setPostcode(String postcode) {
         this.postcode = postcode;
     }
 
-    public void setPhonenumber(long phonenumber){
+    /**
+     * Method to set the users phone number
+     *
+     * @param phonenumber
+     */
+    public void setPhonenumber(long phonenumber) {
         this.phonenumber = phonenumber;
     }
 
-
-    public void addUserToFavourites(User u){
-        if(u != this){
+    /**
+     * Method to add a user to this user's favourites
+     *
+     * @param u - user
+     */
+    public void addUserToFavourites(User u) {
+        if (u != this) {
             this.favouriteUsers.add(u);
         }
     }
 
-    public void removeUserFromFavourites(User u){
-        try{
+    /**
+     * Method to remove a user from this user's favourites
+     *
+     * @param u - user
+     */
+    public void removeUserFromFavourites(User u) {
+        try {
             this.favouriteUsers.remove(u);
-        }
-        catch(NullPointerException e){
+        } catch (NullPointerException e) {
             throw new NullPointerException("Error, user not found in favourites");
         }
     }
 
-    public ArrayList<User> getFavouriteUsers(){
+    /**
+     * Method to get this user's favourites
+     *
+     * @return ArrayList of users
+     */
+    public ArrayList<User> getFavouriteUsers() {
         return this.favouriteUsers;
     }
 
-    public String getFavouritesString(){
+    /**
+     * Method to get a string containing the usernames of this user's favourites
+     *
+     * @return String of favourite user's usernames
+     */
+    public String getFavouritesString() {
         String output = "";
-        for(User e : favouriteUsers){
+        for (User e : favouriteUsers) {
             output = e.getUsername() + " " + output;
         }
         return output;
     }
 
-    public String getTextFileOutput(){
+    /**
+     * Method to get the text file output which will be used to store the user's information
+     *
+     * @return String textFileOutput
+     */
+    public String getTextFileOutput() {
         String output = this.username + "," + this.firstName + "," + this.lastName +
                 "," + this.address + "," + this.phonenumber + "," + this.postcode;
         return output;
     }
 
-    public String toString(){
+    /**
+     * Method to get a string containing all of the user's information
+     *
+     * @return String toString
+     */
+    public String toString() {
         String output = this.username + ": \n" +
                 "Name: " + this.getFullName() + "\n" +
                 "Address: " + this.getAddress() + "\n" +
@@ -122,11 +223,4 @@ public class User
     }
 
 
-
-
-
-
-
-
-
-}
+} //end of class
