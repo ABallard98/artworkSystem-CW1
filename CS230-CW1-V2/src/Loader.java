1bcb5e0e0113483daecb671f6e3b13d9d8a18ae7
ElMarko1 <marcin.kolber1@gmail.com>
2017-11-26 01:11:20 +0000

    new loader class with methods to be implemented

---
+++ CS230-CW1-V2/src/Loader.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.util.ArrayList;
+
+public class Loader {
+
+	private ArrayList<File> userFiles;
+	private ArrayList<File> artworkList;
+	private static ArrayList<User> users;
+	private static ArrayList<Artwork> artworks;
+
+	public static void readUserFiles() {
+
+	}
+
+	
+	
+	
+	/*
+	 * 
+	 * + readUserFile( filename : String) : void 
+	 * + getSelectedUsers(name: String ): ArrayList<User> 
+	 * + getSelectedArtworks(name: String) ArrayList<Artwork>
+	 * + getNewUsers() : ArrayList<User> 
+	 * + getNewArtworks() : ArrayList<Artwork> 
+	 * + getFavouriteArtworks(username: String) : ArrayList<Artwork> 
+	 * +getFavouriteUsers(username: String) : ArrayList<User> 
+	 * - readDataFromUserFile( in : Scanner) : ArrayList<User> 
+	 * + readArtworkFile( filename : String) : void
+	 * - readDataFromArtworkFile( in : Scanner) : ArrayList<Artwork> 
+	 * + readBidFile(filename : String) : void 
+	 * - readFromBidFile(in : Scanner) : ArrayList<Bid> 
+	 * + getUserBidHistory(filename : String, username : String) : ArrayList<Bid>
+	 * 
+	 * 
+	 */
+
+}
