34c5904024b3e53bf190d2f146440818adfac51a
ElMarko1 <marcin.kolber1@gmail.com>
2017-11-26 23:24:11 +0000

    adding methods to be implemented

--- CS230-CW1-V2/src/Loader.java
+++ CS230-CW1-V2/src/Loader.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Loader {
 
@@ -9,28 +10,60 @@ public class Loader {
 	private static ArrayList<Artwork> artworks;
 
 	public static void readUserFiles() {
-		System.out.print("");
+
+		// to implement
+	}
+
+	public void readUserFile(String filename) {
+		// to implement
+	}
+
+	public ArrayList<User> getSelectedUsers(String name) {
+		// to implement
+		return null;
+	}
+
+	public ArrayList<Artwork> getSelectedArtworks(String name) {
+		return null;
 
 	}
 
+	public ArrayList<User> getNewUsers() {
+		return null;
+
+	}
+
+	public ArrayList<Artwork> getFavouriteArtworks(String username) {
+		return null;
+
+	}
+
+	public ArrayList<Artwork> getFavouriteUsers(String username) {
+		return null;
+
+	}
+
+	public ArrayList<User> readDataFromUserFile(Scanner in) {
+		return null;
+
+	}
+
+	public void readArtworkFile(String filename) {
+		// to implement
+	} 
 	
 	
 	
 	/*
 	 * 
-	 * + readUserFile( filename : String) : void 
-	 * + getSelectedUsers(name: String ): ArrayList<User> 
-	 * + getSelectedArtworks(name: String) ArrayList<Artwork>
-	 * + getNewUsers() : ArrayList<User> 
-	 * + getNewArtworks() : ArrayList<Artwork> 
-	 * + getFavouriteArtworks(username: String) : ArrayList<Artwork> 
-	 * +getFavouriteUsers(username: String) : ArrayList<User> 
-	 * - readDataFromUserFile( in : Scanner) : ArrayList<User> 
-	 * + readArtworkFile( filename : String) : void
-	 * - readDataFromArtworkFile( in : Scanner) : ArrayList<Artwork> 
-	 * + readBidFile(filename : String) : void 
-	 * - readFromBidFile(in : Scanner) : ArrayList<Bid> 
-	 * + getUserBidHistory(filename : String, username : String) : ArrayList<Bid>
+	 * 
+	 * 
+	 * +getFavouriteUsers(username: String) : ArrayList<User> -
+	 * readDataFromUserFile( in : Scanner) : ArrayList<User> + readArtworkFile(
+	 * filename : String) : void - readDataFromArtworkFile( in : Scanner) :
+	 * ArrayList<Artwork> + readBidFile(filename : String) : void -
+	 * readFromBidFile(in : Scanner) : ArrayList<Bid> + getUserBidHistory(filename :
+	 * String, username : String) : ArrayList<Bid>
 	 * 
 	 * 
 	 */
