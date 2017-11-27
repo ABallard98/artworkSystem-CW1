44884375b8788001d4c15816daa59d8df49556b0
ABallard98 <aydenballard@gmail.com>
2017-11-27 20:38:03 +0000

    removed unused imports on PaintWindow

--- src/PaintWindow.java
+++ src/PaintWindow.java
@@ -13,20 +13,6 @@ import javafx.stage.Stage;
 import java.util.Random;
 
 
- import java.util.Random;
-
- import javafx.application.Application;
- import javafx.geometry.Insets;
- import javafx.scene.Scene;
- import javafx.scene.canvas.Canvas;
- import javafx.scene.canvas.GraphicsContext;
- import javafx.scene.control.Button;
- import javafx.scene.layout.BorderPane;
- import javafx.scene.layout.Pane;
- import javafx.scene.layout.VBox;
- import javafx.scene.paint.Color;
- import javafx.stage.Stage;
-
 /**
  * TODO CLASS HEADER
  */
