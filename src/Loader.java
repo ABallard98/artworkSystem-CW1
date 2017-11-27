b3266894180e006659b8cc87c9e49daaaabfc433
ABallard98 <aydenballard@gmail.com>
2017-11-27 17:22:57 +0000

    fixed directories

---
+++ src/Loader.java
@@ -0,0 +1,81 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Loader {
+
+	private ArrayList<File> userFiles;
+	private ArrayList<File> artworkList;
+	private static ArrayList<User> users;
+	private static ArrayList<Artwork> artworks;
+
+	public static void readUserFiles() {
+
+		// to implement
+	}
+
+	public void readUserFile(String filename) {
+		// to implement
+	}
+
+	public ArrayList<User> getSelectedUsers(String name) {
+		// to implement
+		return null;
+	}
+
+	public ArrayList<Artwork> getSelectedArtworks(String name) {
+		return null;
+
+	}
+
+	public ArrayList<User> getNewUsers() {
+		return null;
+
+	}
+
+	public ArrayList<Artwork> getFavouriteArtworks(String username) {
+		return null;
+
+	}
+
+	public ArrayList<Artwork> getFavouriteUsers(String username) {
+		return null;
+
+	}
+
+	public ArrayList<User> readDataFromUserFile(Scanner in) {
+		return null;
+
+	}
+
+	public void readArtworkFile(String filename) {
+		// to implement
+	}
+
+	public void readBidFile(String filename) {
+
+	}
+
+	public void readDataFromArtworkFile(String filename) {
+
+	}
+
+	public ArrayList<Bid> getUserBidHistory(String filename, String username) {
+		return null;
+
+	}
+
+	/*
+	 * 
+	 * 
+	 * 
+	 * readDataFromUserFile( in : Scanner) : ArrayList<User> + readArtworkFile(
+	 * filename : String) : void - readDataFromArtworkFile( in : Scanner) :
+	 * ArrayList<Artwork> + readBidFile(filename : String) : void -
+	 * readFromBidFile(in : Scanner) : ArrayList<Bid> + getUserBidHistory(filename :
+	 * String, username : String) : ArrayList<Bid>
+	 * 
+	 * 
+	 */
+
+}
---
+++ src/LoginWindow.java
@@ -0,0 +1,44 @@
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+
+
+
+public class LoginWindow extends Application {
+
+	
+
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
+
+		Scene scene = new Scene(root, 600, 400);
+
+		stage.setTitle("Log in");
+		stage.setScene(scene);
+
+		
+		//loginButton.setOnAction(e-> handleLogin());
+		
+		stage.show();
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	
+	public void handleLogin() {
+		System.out.println(1);
+		
+	}
+	
+}
