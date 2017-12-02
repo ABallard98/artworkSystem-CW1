a4077bafb823c7ae2ba7683a2dace0ef60de51a8
ElMarko1 <marcin.kolber1@gmail.com>
2017-12-02 13:32:41 +0000

    1

--- src/AvatarDrawingController.java
+++ src/AvatarDrawingController.java
@@ -3,6 +3,8 @@ import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 public class AvatarDrawingController {
 
@@ -13,22 +15,55 @@ public class AvatarDrawingController {
     public void initialize() {
     
     	GraphicsContext gc = canvas.getGraphicsContext2D();
-    	draw(gc);
+    	drawCircle(gc);
     	
     	
-    	canvas.addEventHandler(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
-    	    @Override
+        canvas.addEventHandler(MouseEvent.MOUSE_PRESSED, 
+                new EventHandler<MouseEvent>(){
+            @Override
             public void handle(MouseEvent event) {
                 gc.beginPath();
                 gc.moveTo(event.getX(), event.getY());
                 gc.stroke();
+                
             }
-    	});
+        });
+
+        canvas.addEventHandler(MouseEvent.MOUSE_DRAGGED, 
+                new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event) {
+                gc.lineTo(event.getX(), event.getY());
+                gc.stroke();
+                gc.closePath();
+                gc.beginPath();
+                gc.moveTo(event.getX(), event.getY());
+            }
+        });
+        
+        canvas.addEventHandler(MouseEvent.MOUSE_RELEASED, 
+                new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event) {
+                gc.lineTo(event.getX(), event.getY());
+                gc.stroke();
+                gc.closePath();
+            }
+        });
 
     }
     
     
-    public void draw(GraphicsContext gc) {
+    
+    
+    public void drawCircle(GraphicsContext gc) {
+    	Circle circle = new Circle();
+    	gc.setFill(Color.YELLOW);
+    	gc.setStroke(Color.YELLOW);
+    	
+    	gc.fill();
+    	
+    	
     	
     }
     
