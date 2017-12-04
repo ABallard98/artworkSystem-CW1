5f0e148ab5708d6b7bc7432b41fd10b4e3ef04f5
Artificial-Grasshopper <33935398+Artificial-Grasshopper@users.noreply.github.com>
2017-12-04 19:24:45 +0000

    .

--- src/User.java
+++ src/User.java
@@ -384,6 +384,10 @@ public class User {
 		this.artSold = artSold;
 	}
 
+	/**
+	 * Method to get a list of artworks won.
+	 * @return ArrayList<Artwork> - artWon.
+	 */
 	public ArrayList<Artwork> getArtWon() {
 		return artWon;
 	}
