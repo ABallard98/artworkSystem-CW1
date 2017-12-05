a4524196febc75ac5596bb211884cbf456343ad3
ElMarko1 <marcin.kolber1@gmail.com>
2017-12-05 23:51:24 +0000

    favourites display properly

--- src/FavouriteUsersController.java
+++ src/FavouriteUsersController.java
@@ -1,12 +1,19 @@
+import java.io.IOException;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class FavouriteUsersController {
 
@@ -24,6 +31,14 @@ public class FavouriteUsersController {
 
 	@FXML
 	private TableColumn<User, ImageView> avatar;
+	
+
+    @FXML
+    private Button displayUser;
+
+    @FXML
+    private Button deleteUser;
+	
 
 	public void initialize() {
 
@@ -34,7 +49,45 @@ public class FavouriteUsersController {
 		table1.setItems(favourites);
 		favUsers.setText(LoginController.getUser().getFavouriteUsers().size() + "");
 
+		deleteUser.setOnAction(e-> deleteUser() );
 
+		displayUser.setOnAction(e-> showUser());
 	}
 
+	public void showUser() {
+		User displayedUser = table1.getSelectionModel().getSelectedItem();
+		
+		
+		if(displayedUser != null) {
+			UserDisplayController.setUser(displayedUser);
+			
+
+			FXMLLoader fxmlL = new FXMLLoader(getClass().getResource("/UserDisplay.fxml"));
+			try {
+				Parent root = fxmlL.load();
+				// NewAccountCreatorController newAccountController =
+				// fxmlL.<NewAccountCreatorController>getController();
+
+				Scene scene = new Scene(root, 450, 300);
+
+				Stage stage = new Stage();
+				stage.setScene(scene);
+				stage.initModality(Modality.APPLICATION_MODAL);
+
+				stage.setTitle(displayedUser.getUsername());
+				stage.show();
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			
+		}
+	}
+	
+	public void deleteUser() {
+		
+	}
+	
 }
